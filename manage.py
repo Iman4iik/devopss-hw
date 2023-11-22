32e78aa9638f31d9370ef77b88944a64678b45cf
Iman4iik <120765924+Iman4iik@users.noreply.github.com>
2023-11-22 14:40:09 +0600

    22.11.2023

--- manage.py
+++ manage.py
@@ -4,6 +4,16 @@ import os
 import sys
 
 
+try:
+    import setuptools
+except ImportError as error:
+    print("ERROR: Can not execute `setup.py` since setuptools is not available in",
+          "the build environment.",
+          file=sys.stderr,
+          )
+    sys.exit(1)
+
+
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Myproject.settings')
@@ -20,3 +30,12 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError:
+        print("ERROR: Django is not installed. Please install it using pip install django`.")
+        sys.exit(1)
+
+    execute_from_command_line(sys.argv)
